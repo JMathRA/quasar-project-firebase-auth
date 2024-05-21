8a7984fe3e1893f2aa29f71f400c773ec42a75ad
JMathRA <j.matheusalbuquerque1@gmail.com>
2024-05-21 08:52:28 -0300

    aplicação de login e registro utilizando firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
